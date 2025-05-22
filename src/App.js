e98b5a98536ff755ff2457bfaee5e2d935412f22
raj patel <kakhilesh79@gmail.com>
2025-05-22 23:54:33 +0530

    logut

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload fast by Arpit.
+          Edit <code>src/App.js</code> and save to reload fast by Arpit and logout by branch.
         </p>
         <a
           className="App-link"
