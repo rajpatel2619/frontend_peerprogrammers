2b7802b43191e684d13e1e54d20b6aae692d6ddb
Dannavv <thearpit1@gmail.com>
2025-05-22 23:47:14 +0530

    testing by arpit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload fast.
+          Edit <code>src/App.js</code> and save to reload fast by Arpit.
         </p>
         <a
           className="App-link"
