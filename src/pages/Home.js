dda6023b13966fc92dfa09c6f16f2238bda48d6c
Raj Patel <47470051+rajpatel2619@users.noreply.github.com>
2025-07-24 22:28:07 +0530

    Sanjana (#7)
    * target changes
    
    * update
    
    ---------
    
    Co-authored-by: SanjanaMATHUR1014 <14sanjanamathur@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import HeroSection  from '../components/home/HeroSection';
 import CTASection from '../components/home/CTASection';
+
+import FAQs from '../components/home/FAQs';
 import Features from '../components/Features';
+
 
 
 const HomePage = () => { // Note: Capitalized component name
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-black">
       <HeroSection />
+
+      <CTASection />
+      {/* <CourseCard /> */}
+      {/* <CourseTabs /> */}
+      <FAQs />
+
       <Features/>
       {/* <CTASection /> */}
       {/* <CourseCard /> */}
       {/* <CourseTabs /> */}
       
+
     </div>
   );
 };
